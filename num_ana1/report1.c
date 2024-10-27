f60bc3bd45c0825937cb17378459bfc4f79eb5f7
k310sto <k310.sto@gmail.com>
2024-10-28 02:08:20 +0900

    Initial Commit

---
+++ num_ana1/report1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(){
+
+	char filename[] = "input.txt";
+	FILE *ip = fopen(filename, "r");
+	FILE *op = fopen("output.txt", "w");
+
+	char letr;
+	while( (letr = fgetc(ip)) != EOF ){
+		if( isalpha(letr) )
+			fprintf(op, "%c", letr);
+		else {
+			switch(letr){
+			case 10 :
+				fprintf(op, "\n");
+				break;
+			case 39 :
+				fprintf(op, "'");
+				break;
+			case 44 :
+				fprintf(op, ",");
+				break;
+			case 46 :
+				fprintf(op, ".");
+				break;
+			default :
+				fprintf(op, " ");
+				break;
+			}
+		}
+	}
+	fclose(ip);
+	fclose(op);
+
+return 0;
+}
---
+++ num_ana1/report3.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int liter_code(char n){		//添え字を文字コードに変換する関数
+	switch(n){
+	case 52 :
+		return 32;
+	case 53 :
+		return 39;
+	case 54 :
+		return 44;
+	case 55 :
+		return 46;
+	default :
+		if( n <= 25 ) 
+			return (n + 65);
+		else if( n <= 51 )
+			return (n + 71);
+		else 
+			return 0;
+	}
+}
+
+
+int main(){
+
+	char filename[] = "output.txt";
+ 
+	char letr;
+
+	struct statis{
+		char code;				//文字識別用
+		unsigned int num;			//出現個数用
+	};
+	struct statis moji[56];
+
+	for(int j = 0; j < 52; j++){			//初期化
+		moji[j].num = 0;
+	}
+
+	FILE *ip = fopen(filename, "r");
+	while( (letr = fgetc(ip)) != EOF ){
+		for( int i = 0; i < 56; i++ ){		//"z"が文字コード122
+			moji[i].code = liter_code(i);	//今から調べる文字のコードを格納
+			if( letr == moji[i].code ){
+				moji[i].num++;
+				break;
+			}
+		}
+	}
+	fclose(ip);
+
+
+	//---バブルソート---//
+	double tmp;
+	for(int i = 0; i < 55; i++ ){
+		for(int j = 0; j < 55; j++){
+			if(moji[j].num < moji[j+1].num){
+				tmp = moji[j].num;
+				moji[j].num = moji[j+1].num;
+				moji[j+1].num = tmp;
+				tmp = moji[j].code;
+				moji[j].code = moji[j+1].code;
+				moji[j+1].code = tmp;
+			}
+		}
+	}
+	for(int i = 0; i < 55; i++){
+		if(moji[i].num > 0)
+			printf("< %c > : %d \n",moji[i].code, moji[i].num);
+
+	}
+return 0;
+}
